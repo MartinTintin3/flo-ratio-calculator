ea4c12611a8cc7c669356a4095717871c60b6a8d
Replit user <>
2024-01-16 18:00:44 +0000

    first test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,31 @@
 <script>
 	// Stuff
+	let url = "";
+	let data_promise = null;
+	
+	const get_data = async () => {
+		// https://www.flowrestling.org/people/9079327-dillon-noonan
+		const raw = await (await fetch(url)).text();
+		const data = (new DOMParser()).parseFromString(raw, "text/html");
+
+		alert(data.body);
+	}
 </script>
 
-<h1>Template</h1>
-<h2>Put Stuff here</h2>
+<input type="url">
+<button type="button" on:click={() => data_promise = get_data(url)}>Fetch</button>
+
+<p>
+	{#if data_promise == null}
+		{#await data_promise}
+			Loading planet...
+		{:then data}
+			Your next planet is {data}.
+		{:catch error}
+			System error: {error.message}.
+		{/await}
+	{/if}
+</p>
 
 <style>
 	* {

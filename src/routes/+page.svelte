be1937722433a03f5b2287156f04c56a241c05f0
MartinTintin3 <maroymart@gmail.com>
2024-01-16 23:03:05 -0500

    e

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,6 @@
 	}
 	
 	const get_data = async () => {
-		// https://www.flowrestling.org/people/9079327-dillon-noonan
 		const headers = new Headers({
 			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
